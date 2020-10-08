2fe76c62caea8db035d93092f8c34b0b22ddeb47
fede22 <fperezfagonde@gmail.com>
2020-10-08 09:26:08 -0300

    Add golden file, test GetProxy service and Test Netblock method

--- internal/proxy/model_test.go
+++ internal/proxy/model_test.go
@@ -1,6 +1,9 @@
 package proxy
 
 import (
+	"encoding/json"
+	"github.com/go-playground/assert/v2"
+	"io/ioutil"
 	"net"
 	"testing"
 )
@@ -19,4 +22,26 @@ func TestIPv6ToDecimalAndBack(t *testing.T) {
 	if !ip.Equal(ip2.IP) {
 		t.Errorf("expected ip %s, got instead %s", ip, ip2)
 	}
+}
+
+func TestNetblock(t *testing.T) {
+	b, err := ioutil.ReadFile("mocks/testdata/proxy_ar.json")
+	if err != nil {
+		t.Fatalf("error loading golden file: %s", err)
+	}
+	var p Proxy
+	err = json.Unmarshal(b, &p)
+	if err != nil {
+		t.Fatalf("error unmarshaling proxy: %s", err)
+	}
+	nb, err := p.Netblock()
+	if err != nil {
+		t.Fatalf("error in NetBlock(): %s", err)
+	}
+	expected := []NetIP{
+		{net.ParseIP("23.237.23.73")},
+		{net.ParseIP("23.237.23.74")},
+		{net.ParseIP("23.237.23.75")},
+	}
+	assert.Equal(t, expected, nb)
 }
